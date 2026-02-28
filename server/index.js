5e4324ddec97e1c102643f5577fd05258c521c11
abdulraziq704 <meharabdulraziq3000@gmail.com>
2026-02-28 19:58:03 +0500

    / removed from url

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://loan-nonfanatical-overdiffusely.ngrok-free.dev',
-    'https://reviewblast.vercel.app/'
+    'https://reviewblast.vercel.app'
 ];
 
 app.use(cors({
